b8747ed58e54d1e7557a1680704f984f743f8f7c
cristhiancepeda <cristhian.cepeda@wearemo.com>
2025-05-09 08:58:49 -0500

    feat(quiz-maker): add filters logic

--- src/app/quiz-maker/quiz-maker.component.ts
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -1,27 +1,76 @@
-import {Component} from '@angular/core';
-import {Category, Difficulty, Question} from '../data.models';
-import {Observable} from 'rxjs';
-import {QuizService} from '../quiz.service';
-import {FormsModule} from '@angular/forms';
-import {QuizComponent} from '../quiz/quiz.component';
-import {AsyncPipe} from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Category, Difficulty, Question } from '../data.models';
+import { filter, map, Observable } from 'rxjs';
+import { QuizService } from '../quiz.service';
+import { FormsModule } from '@angular/forms';
+import { QuizComponent } from '../quiz/quiz.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-quiz-maker',
   imports: [FormsModule, QuizComponent, AsyncPipe],
   templateUrl: './quiz-maker.component.html',
-  styleUrls: ['./quiz-maker.component.css']
+  styleUrls: ['./quiz-maker.component.css'],
 })
-export class QuizMakerComponent {
-
+export class QuizMakerComponent implements OnInit {
   categories$: Observable<Category[]>;
+  formatedCategories$: Observable<Category[]>;
   questions$!: Observable<Question[]>;
+  subCategories$: Observable<Category[]>;
+  selectedCategoryId: string;
 
-  constructor(protected quizService: QuizService) {
-    this.categories$ = quizService.getAllCategories()
+  protected _quizService = inject(QuizService);
+
+  ngOnInit(): void {
+    this._setInitialValues();
   }
 
-  createQuiz(cat: string, difficulty: string): void {
-    this.questions$ = this.quizService.createQuiz(cat, difficulty as Difficulty);
+  public onCreateQuiz(cat: string, difficulty: string): void {
+    this.questions$ = this._quizService.createQuiz(
+      cat,
+      difficulty as Difficulty
+    );
+  }
+
+  public onSelectCategory(categoryId: string): void {
+    this.subCategories$ = this.categories$.pipe(
+      map((categories) => {
+        const filteredCategories = categories.filter((category) => {
+          const selectedCategory =
+            categories
+              .find((cat) => cat.id === parseInt(categoryId))
+              ?.name.split(':')[0] || '';
+          return category.name.includes(selectedCategory);
+        });
+        if (filteredCategories.length === 1) {
+          this.selectedCategoryId = filteredCategories[0].id.toString();
+        }
+        return filteredCategories;
+      })
+    );
+  }
+
+  public onSelectSubCategory(categoryId: string) {
+    this.selectedCategoryId = categoryId;
+  }
+
+  private _setInitialValues() {
+    this.categories$ = this._quizService.getAllCategories();
+    this.formatedCategories$ = this.categories$.pipe(
+      map((categories) => {
+        const newCategories = categories.map((category) => {
+          const formatedCategory = {
+            name: category.name.split(':')[0],
+            id: category.id,
+          };
+          return formatedCategory;
+        });
+        return Array.from(
+          new Map(
+            newCategories.map((category) => [category.name, category])
+          ).values()
+        );
+      })
+    );
   }
 }
